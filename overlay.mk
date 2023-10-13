4d568eafe074aae48a3fa42dec8c0304a3751c6a
Zannatun Nayem <znayem830@gmail.com>
2023-10-13 20:37:36 +0600

    first commit

---
+++ overlay.mk
@@ -0,0 +1,32 @@
+PRODUCT_PACKAGES += \
+	HardwareOverlayPicker \
+	QtiAudio \
+	TrebleApp \
+	treble-overlay-NavBar \
+	treble-overlay-NightMode \
+	treble-overlay-SystemUI-FalseLocks \
+	treble-overlay-Telephony-LTE \
+	treble-overlay-caf-ims \
+	treble-overlay-devinputjack \
+	treble-overlay-highpriomisc \
+	treble-overlay-hw-ims \
+	treble-overlay-lge-mfh505glm \
+	treble-overlay-lge-mfh505glm-systemui \
+	treble-overlay-lge-timelm \
+	treble-overlay-misc-aod \
+	treble-overlay-misc-aod-systemui \
+	treble-overlay-misc-dt2w \
+	treble-overlay-misc-minimal-brightness \
+	treble-overlay-mtk-ims \
+	treble-overlay-sec-ims \
+	treble-overlay-sprd-ims \
+	treble-overlay-telephony-caf-ims \
+	treble-overlay-telephony-hw-ims \
+	treble-overlay-telephony-mtk-ims \
+	treble-overlay-telephony-slsi-ims \
+	treble-overlay-telephony-sprd-ims \
+	treble-overlay-tethering \
+	treble-overlay-tethering-nobpf \
+	treble-overlay-wifi5g \
+
+
